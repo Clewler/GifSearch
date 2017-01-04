585bce6355fdf3f7e7336c5244ea851231bb8045
Clewler <eurobi.123@gmail.com>
2017-01-04 16:37:53 +0100

    test1

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>PhpFiddle Initial Code</title>
+
+<script type="text/javascript" src="/js/jquery/1.7.2/jquery.min.js"></script>
+
+	
+
+
+</head>
+
+<body>
+<form action="" method="GET">
+<input type="text" name="query" value='
+<?php
+if(isset($_GET['query']))
+	echo $_GET['query'];
+?>'/>
+<button type="submit">Wyslij</button>
+<br/>
+
+
+</body>
+	<?php
+	if(!file_exists('data.txt'))
+		{
+			$fp = fopen('data.txt','w+');
+			fclose($fp);
+		}
+	/*$file = file_get_contents( 'data.txt' );
+	$dopisz = 'Jakis tam tekst';
+	file_put_contents( 'data.txt', $dopisz . $file );
+	file_put_contents( 'data.txt', $dopisz . $file );
+	print_r($file);*/
+	
+if(isset($_GET['query']))
+{	
+	
+	
+	$url = "http://api.giphy.com/v1/gifs/search?q=".urlencode($_GET['query'])."&api_key=dc6zaTOxFJmzC";
+	$a=file_get_contents($url);
+	$b=json_decode($a, true); //Przetwarzanie oraz dekodowanie danych z aplikacji
+	//print_r($b['data'][0]['id']);
+
+if(!empty($b['data']) && $b['meta']['status']==200)
+{	
+	$it=0;
+	$file = file_get_contents('data.txt');
+	$data = explode(",", $file);
+	$temp=NULL;
+	do
+	{
+
+		if(!array_search($b['data'][$it]['id'],$data))
+		{
+			$temp.=','.$b['data'][$it]['id'].',0';
+		}
+	$it++;
+	}while(!empty($b['data'][$it]));
+	
+	if(!empty($temp))
+	{	
+		file_put_contents('data.txt', $temp . $file);
+	}
+	$file = file_get_contents('data.txt');
+	$data = explode(",", $file);
+	$i=0;
+	do
+	{
+		if(isset($_GET[$b['data'][$i]['id']]))
+		{
+			$value = explode(',',$_GET[$b['data'][$i]['id']]);
+			if($value[1]=='-1')
+			{
+				$data[array_search($b['data'][$i]['id'],$data)+1]++;
+			}
+			else if($value[1]=='-0')
+			{
+				$data[array_search($b['data'][$i]['id'],$data)+1]--;
+			}
+			else
+			{	
+				
+				header("Location: http://niemam.000webhostapp.com/");
+				
+			}
+			header("Location: http://niemam.000webhostapp.com/?query=".urlencode($_GET['query']));
+			file_put_contents('data.txt',implode(',',$data));
+		}
+		echo '<img src="'.$b['data'][$i]['images']['original']['url'].'"/><br/>';
+		echo '<button type="submit"  name="'.$b['data'][$i]['id'].'" value="'.$b['data'][$i]['id'].',-1"">Lubie to!</button>';
+		echo $data[array_search($b['data'][$i]['id'],$data)+1];
+		echo '<button type="submit"  name="'.$b['data'][$i]['id']. '"value="'.$b['data'][$i]['id'].',-0""/>Nie lubie</button>';
+		echo '<br/><br/><br/><br/><br/><br/>';
+		$i++;
+
+	}while(!empty($b['data'][$i]));
+
+}
+else if($b['meta']['status']==403)
+{
+	echo '<p>Blad polaczenia z baza danych, sprobuj ponownie pozniej</p>';
+}
+else
+{
+	echo '<p>Nie mznaleziono podanej frazy</p>';
+}
+}
+	
+?>
+</form>
+</html>
